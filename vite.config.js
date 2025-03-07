90c0fff6ae088489ef2f4d0cf772e73c322a3db3
ra <ra@gmail.com>
2025-03-07 20:26:03 +0500

    test3

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
     base: '/game-stor/',
+    plugins: [react()],
     build: {
         rollupOptions: {
             output: {
